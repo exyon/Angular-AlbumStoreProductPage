334f744872e9f73149224351388602fcb8bf3d34
Jomon Vachas <jomon@live.com>
2018-04-09 23:15:50 -0400

    Adding the Tracklisting Component - part1 fix 2

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Album} from '../album';
-import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -10,12 +8,10 @@ import {ProductService} from '../product.service';
 })
 export class ProductPageComponent implements OnInit {
 
-  albumInfo: Album;
-
-  constructor(private _productService: ProductService) { }
+  constructor() { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  
   }
 
 }
